04b6eaa83a8c88e0b6f726223cb0d81b01aff016
Ryan Murphy <56881027+RyanMurph17@users.noreply.github.com>
2020-03-02 17:29:47 +0000

    Add files via upload

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid d-flex flex-wrap justify-content-between">
+      <nav>
+        <ul>
+          <li>
+            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright d-flex flex-wrap">
+        &copy; Sportly - Social Media Dashboard</a>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
