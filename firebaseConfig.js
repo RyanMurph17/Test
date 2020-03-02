2c279f76e3fb6227c46b135caa87fd82a07e8170
Ryan Murphy <56881027+RyanMurph17@users.noreply.github.com>
2020-03-02 17:24:01 +0000

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBaWJ2p6TDNq1WuOVLLtjsJq1xDypaXqdM",
+  authDomain: "sportlydatabase.firebaseapp.com",
+  databaseURL: "https://sportlydatabase.firebaseio.com",
+  projectId: "sportlydatabase",
+  storageBucket: "sportlydatabase.appspot.com",
+  messagingSenderId: "287835560944"
+};
